8bbc813bc6532a31893a2f1fbd21b92f2f1c2ac6
AldoVasquez07 <avasquezl@ulasalle.edu.pe>
2025-11-25 09:31:26 -0500

    Ajustes de login

--- crud-habitaciones/src/views/LoginView.vue
+++ crud-habitaciones/src/views/LoginView.vue
@@ -54,7 +54,7 @@ export default {
 
         const login = async () => {
             try {
-                const res = await axios.post('http://localhost:5000/api/auth/login', {
+                const res = await axios.post(`${import.meta.env.VITE_API_URL}/auth/login`, {
                     email: email.value,
                     password: password.value
                 });
